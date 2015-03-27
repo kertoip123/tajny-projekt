9d65f0c69dbc18b9ccc52b1434848bce4f264321
kamilfocus <k4mil.piszczek@gmail.com>
2015-03-27 23:43:51 +0100

    wydrukdokonsoli

--- src/ProblemInstance.java
+++ src/ProblemInstance.java
@@ -24,8 +24,19 @@ public class ProblemInstance {
     }
 
     public void createDistanceMatrix(){
-        distance = new int[deliveryPoints.length+magazines.length][deliveryPoints.length+magazines.length];
-
+        int total = deliveryPoints.length+magazines.length;
+        distance = new int[total][total];
+        int i;
+        for(i=0;i<deliveryPoints.length;i++){
+            int j;
+            for(j=0;j<deliveryPoints.length;j++){
+                distance[i][j]=pitagoras(deliveryPoints[i].x,deliveryPoints[i].y,deliveryPoints[j].x,deliveryPoints[j].y);
+            }
+            for(;j<total;j++){
+                distance[i][j]=pitagoras(deliveryPoints[i].x,deliveryPoints[i].y,magazines[j-deliveryPoints.length].x,magazines[j-deliveryPoints.length].y);
+            }
+            // TODO sort for deliveryPoint[i] neighbourhood list
+        }
     }
 
     int pitagoras(int x1, int y1, int x2, int y2){
